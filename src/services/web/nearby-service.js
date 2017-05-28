a7249e27cc808c150c18b4a404e05e77a2079d09
joebui <dienbuijoe@outlook.com>
2017-05-28 08:11:28 +0700

    setup redis

--- src/services/web/nearby-service.js
+++ src/services/web/nearby-service.js
@@ -1,18 +1,33 @@
 const Promise = global.Promise
 
+const redis = require('../../DAL/redis-connection')
 const nearbyRepo = require('../../DAL/repositories/nearby-repository')
 
 class NearbyService {
     getNearbyRecord(city, district) {
+        const key = city + '-' + district
         return new Promise((resolve, reject) => {
-            nearbyRepo.getNearbyRecord(city, district, '-_id nearby')
-                .then(nearby => {
-                    resolve(nearby.nearby)
-                })
-                .catch(err => {
+            // Check data in redis.
+            redis.get(key, (err, reply) => {
+                if (err) {
+                    // Error.
                     reject({ message: err })
-                })
-        });
+                } else if (reply) {
+                    // Data exists.
+                    resolve(JSON.parse(reply))
+                } else {
+                    // Get data from mongo and store in redis.
+                    nearbyRepo.getNearbyRecord(city, district, '-_id nearby')
+                        .then(nearby => {
+                            redis.set(key, JSON.stringify(nearby.nearby))
+                            resolve(nearby.nearby)
+                        })
+                        .catch(err => {
+                            reject({ message: err })
+                        })
+                }
+            })
+        })
     }
 }
 
