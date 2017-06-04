c8fb64addd4ee3d3941e51f39e22c466d39a2a3e
joebui <dienbuijoe@outlook.com>
2017-06-04 23:18:16 +0700

    change db schema

--- src/DAL/models/record-model.js
+++ src/DAL/models/record-model.js
@@ -2,8 +2,17 @@ const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema(
     {
-        deviceID: 'string', utcDateTime: 'string', no: 'number', so2: 'number', pm2: 'number', 
-        pm10: 'number', o3: 'number', co: 'number', sound: 'number', uv: 'number'
+        deviceID: 'string',
+        utcDateTime: 'string',
+        no: 'number', noAQI: 'number',
+        so2: 'number', so2AQI: 'number',
+        pm2: 'number', pm2AQI: 'number',
+        pm10: 'number', pm10AQI: 'number',
+        o3: 'number', o3AQI: 'number',
+        co: 'number', coAQI: 'number',
+        sound: 'number',
+        uv: 'number',
+        aqi: 'number'
     },
     { collection: 'records' }
 )
