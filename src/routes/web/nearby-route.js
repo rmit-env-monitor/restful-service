b445e9d8cf19ca02c2670ba27c0814da598379a8
joebui <dienbuijoe@outlook.com>
2017-07-15 15:03:31 +0700

    update http status code for error

--- src/routes/web/nearby-route.js
+++ src/routes/web/nearby-route.js
@@ -12,7 +12,7 @@ module.exports = app => {
                 res.status(200).json(nearby)
             })
             .catch(err => {
-                res.status(400).json(err)
+                res.status(500).json(err)
             })
     })
 
