df493d42369f2da70e832c524b869c182645bf8b
joebui <dienbuijoe@outlook.com>
2017-08-24 00:02:10 +0700

    add API for station ranking for mobile v1

---
+++ src/routes/mobile/v1/background-job-route.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+
+const backgroundJobService = require('../../../services/mobile/v1/background-job-service')
+
+module.exports = app => {
+    router.get('/station-ranking', (req, res) => {
+        backgroundJobService.getStationRanking(req.query.city, req.query.deviceId).then(result => {
+            res.status(200).json(result)
+        })
+    })
+
+    app.use('/api/mobile/v1', router)
+}
