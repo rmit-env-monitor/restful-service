76d4c3c0942fc47e59a4e1c596d6ca6ca2a39fdd
joebui <dienbuijoe@outlook.com>
2017-11-25 14:37:45 +0700

    move nearby data to json file

--- src/app/nearby/web/nearby-service.js
+++ src/app/nearby/web/nearby-service.js
@@ -1,33 +1,11 @@
-const Promise = global.Promise
-const redis = global.redis
-
-const nearbyRepo = require('../../../DAL/repositories/nearby-repository')
-const constants = require('../../../utilities/constants')
+const jsonQuery = require('json-query')
 
 class NearbyService {
-    async getNearbyRecord(city, district) {
-        const key = city + '_' + district + '_nearby'
-        const redisData = await this._retrieveRedisData(key)
-
-        if (redisData.err) {
-            return { message: redisData.err }
-        } else if (redisData.reply) {
-            return JSON.parse(redisData.reply)
-        } else {
-            const nearby = await nearbyRepo.getNearbyRecord(city, district, constants.MONGOOSE_QUERY.NEARBY)
-            redis.set(key, JSON.stringify(nearby.nearby))
-            redis.expire(key, constants.ONE_WEEK_EXPIRE)
-            return nearby.nearby
-        }
-    }
-
-    _retrieveRedisData(key) {
-        return new Promise((resolve, reject) => {
-            redis.get(key, (err, reply) => {
-                resolve({ err: err, reply: reply })
-            })
-        })
-    }
+  async getNearbyRecord(city, district) {
+    const data = require('../../../utilities/nearby-data.json')
+    const value = jsonQuery(`[*city=${city} & district=${district}]`, { data }).value
+    return value[0].nearby
+  }
 }
 
 module.exports = new NearbyService()
