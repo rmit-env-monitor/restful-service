31f8a3d584a2e875e49acd7bf1984b1380233480
joebui <dienbuijoe@outlook.com>
2017-12-11 21:41:38 +0700

    include email in register

--- src/DAL/models/user-model.js
+++ src/DAL/models/user-model.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema(
-    {
-        username: String,
-        password: String
-    },
-    {
-        collection: 'users'
-    }
+  {
+    username: String,
+    password: String,
+    email: String
+  },
+  {
+    collection: 'users'
+  }
 )
 module.exports = mongoose.model('users', schema)
--- src/app/user/shared/auth-service.js
+++ src/app/user/shared/auth-service.js
@@ -5,49 +5,49 @@ const tokenGenerator = require('./auth/local/token-generator')
 const constants = require('../../../utilities/constants')
 
 class AuthService {
-    async registerUser(body) {
-        const value = {}
+  async registerUser(body) {
+    const value = {}
 
-        /** Check if username exists */
-        const isUserAvailable = await authRepo.authenticate({ username: body.username.trim() })
-        if (isUserAvailable) {
-            value.message = 'Username is already taken. Please try again!'
-            return value
-        } else {
-            /** Hash password */
-            const password = await passwordHashing(body.password.trim())
-            body.password = password
+    /** Check if username exists */
+    const isUserAvailable = await authRepo.authenticate({ username: body.username.trim() })
+    if (isUserAvailable) {
+      value.message = 'Username is already taken. Please try again!'
+      return value
+    } else {
+      /** Hash password */
+      const password = await passwordHashing(body.password.trim())
+      body.password = password
 
-            /** Add new user to DB */
-            await authRepo.registerAccount(body)
-            value.username = body.username
-            value.token = tokenGenerator(value)
-            return value
-        }
+      /** Add new user to DB */
+      await authRepo.registerAccount(body)
+      value.username = body.username
+      value.token = tokenGenerator(value)
+      return value
     }
+  }
 
-    async login(body) {
-        const value = {}
+  async login(body) {
+    const value = {}
 
-        /** Check if username exists */
-        const user = await authRepo.authenticate({ username: body.username.trim() })
-        if (!user) {
-            /** Username not found */
-            value.message = constants.ERROR_MESSAGE
-            return value
-        } else {
-            /** Check password */
-            const passwordCheckResult = await passwordChecking(body.password, user.password)
-            if (passwordCheckResult) {
-                value.username = user.username
-                value.token = tokenGenerator(value)
-                return value
-            } else {
-                value.message = constants.ERROR_MESSAGE
-                return value
-            }
-        }
+    /** Check if username exists */
+    const user = await authRepo.authenticate({ username: body.username.trim() })
+    if (!user) {
+      /** Username not found */
+      value.message = constants.ERROR_MESSAGE
+      return value
+    } else {
+      /** Check password */
+      const passwordCheckResult = await passwordChecking(body.password, user.password)
+      if (passwordCheckResult) {
+        value.username = user.username
+        value.token = tokenGenerator(value)
+        return value
+      } else {
+        value.message = constants.ERROR_MESSAGE
+        return value
+      }
     }
+  }
 }
 
 module.exports = new AuthService()
